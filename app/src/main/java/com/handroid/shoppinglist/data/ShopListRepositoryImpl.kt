aea173accc284d311b9b69a4090ce9ec8d7cb46c
A1MaHaiD <super.lukianchuk2010@gmail.com>
2022-01-04 13:25:27 +0200

    added dependency @Room and plugin @kotlin-kapt && created ShopListRepositoryImpl.kt

---
+++ app/src/main/java/com/handroid/shoppinglist/data/ShopListRepositoryImpl.kt
@@ -0,0 +1,26 @@
+package com.handroid.shoppinglist.data
+
+import com.handroid.shoppinglist.domain.ShopItem
+import com.handroid.shoppinglist.domain.ShopListRepository
+
+object ShopListRepositoryImpl:ShopListRepository {
+    override fun addShopItem(shopItem: ShopItem) {
+        TODO("Not yet implemented")
+    }
+
+    override fun removeShopItem(shopItem: ShopItem) {
+        TODO("Not yet implemented")
+    }
+
+    override fun editShopItem(shopItem: ShopItem) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getShopItem(shopItemId: Int): ShopItem {
+        TODO("Not yet implemented")
+    }
+
+    override fun getShopList(): List<ShopItem> {
+        TODO("Not yet implemented")
+    }
+}
