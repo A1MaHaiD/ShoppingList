44e89f5b8204500a67ba28f0a39ef97611f65748
A1MaHaiD <super.lukianchuk2010@gmail.com>
2022-01-04 14:38:54 +0200

    created AppDatabase.kt && ShopListDao.kt && added annotation

--- app/src/main/java/com/handroid/shoppinglist/domain/ShopItem.kt
+++ app/src/main/java/com/handroid/shoppinglist/domain/ShopItem.kt
@@ -1,7 +1,13 @@
 package com.handroid.shoppinglist.domain
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "shop_list")
 data class ShopItem(
+    @PrimaryKey
     val id:Int,
+    @PrimaryKey
     val name:String,
     val count: Int,
     val isSelected:Boolean
