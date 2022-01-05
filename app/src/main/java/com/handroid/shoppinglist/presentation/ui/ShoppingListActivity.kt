cd40f4468fe8edd766b0fc11b32c2f1fe8079b26
A1MaHaiD <super.lukianchuk2010@gmail.com>
2022-01-05 17:45:11 +0200

    created custom List<LinearLayout> showList() added layout activity_shopping_list.xml && item_shop_selected.xml && item_shop_unselected.xml

--- app/src/main/java/com/handroid/shoppinglist/presentation/ui/ShoppingListActivity.kt
+++ app/src/main/java/com/handroid/shoppinglist/presentation/ui/ShoppingListActivity.kt
@@ -2,32 +2,42 @@ package com.handroid.shoppinglist.presentation.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import android.view.LayoutInflater
+import android.widget.LinearLayout
 import androidx.lifecycle.ViewModelProvider
+import com.handroid.shoppinglist.R
 import com.handroid.shoppinglist.databinding.ActivityShoppingListBinding
+import com.handroid.shoppinglist.domain.ShopItem
 import com.handroid.shoppinglist.presentation.viewmodel.ShoppingListViewModel
 
 class ShoppingListActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityShoppingListBinding
     private lateinit var viewModel: ShoppingListViewModel
-    private var count = 0
+//    private lateinit var llShopList:LinearLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityShoppingListBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+//        binding.llShopList
 
         viewModel = ViewModelProvider(this)[ShoppingListViewModel::class.java]
         viewModel.shopList.observe(this) {
-            Log.i("ShoppingListActivity", "element $it")
-            if (count == 0) {
-                count++
-                val item = it[0]
-                viewModel.changeSelectedState(item)
-                Log.i("ChangeTest", "change element $it")
+            showList(it)
+        }
+    }
+
+    fun showList(list: List<ShopItem>){
+        for(shopItem in list){
+            val layoutId = if (shopItem.isSelected){
+                R.layout.item_shop_selected
+            } else {
+                R.layout.item_shop_unselected
             }
+            val view = LayoutInflater.from(this).inflate(layoutId,binding.llShopList, false)
+            binding.llShopList.addView(view)
         }
     }
 }
