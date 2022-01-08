7542a6d178b6e989446b825b76d654f537589acd
A1MaHaiD <super.lukianchuk2010@gmail.com>
2022-01-08 22:33:42 +0200

    some fix && execute ShopItemViewModel.kt
    Unfinished @ViewModel!!!!

--- app/src/main/java/com/handroid/shoppinglist/presentation/view_models/ShopItemViewModel.kt
+++ app/src/main/java/com/handroid/shoppinglist/presentation/view_models/ShopItemViewModel.kt
@@ -3,24 +3,41 @@ package com.handroid.shoppinglist.presentation.view_models
 import androidx.lifecycle.ViewModel
 import com.handroid.shoppinglist.data.ShopListRepositoryImpl
 import com.handroid.shoppinglist.domain.ShopItem
+import com.handroid.shoppinglist.domain.list.AddShopItemUseCase
 import com.handroid.shoppinglist.domain.list.EditShopItemUseCase
-import com.handroid.shoppinglist.domain.list.GetShopListUseCase
+import com.handroid.shoppinglist.domain.list.GetShopItemUseCase
 
-class ShopItemViewModel:ViewModel() {
+class ShopItemViewModel : ViewModel() {
 
-    private val  repository = ShopListRepositoryImpl
+    private val repository = ShopListRepositoryImpl
 
-    private val getShopElementUseCase = GetShopListUseCase(repository)
+    private val getShopElementUseCase = GetShopItemUseCase(repository)
+    private val addShopItemUseCase = AddShopItemUseCase(repository)
     private val editShopElementUseCase = EditShopItemUseCase(repository)
-    private val saveShopElementUseCase = EditShopItemUseCase(repository)
 
-    val shopList = getShopElementUseCase.getShopList()
+    fun getShopItem(shopItemId: Int) {
+        val item = getShopElementUseCase.getShopItem(shopItemId)
+    }
 
-    fun editShopItem(shopItem:ShopItem){
+    fun addShopItem(inputName: String?, inputCount: String?) {
+        val name = parseName(inputName)
+        val count = parseCount(inputCount)
+        addShopItemUseCase.addShopItem(name, count)
+    }
+
+    fun editShopItem(shopItem: ShopItem) {
         editShopElementUseCase.editShopItem(shopItem)
     }
 
-    fun  saveShopItem(shopItem: ShopItem){
-        saveShopElementUseCase.editShopItem(shopItem)
+    private fun parseName(inputName: String?): String {
+        return inputName?.trim() ?: ""
+    }
+
+    private fun parseCount(inputCount: String?): Int {
+        return try {
+            inputCount?.trim()?.toInt() ?: 0
+        } catch (e: Exception) {
+            0
+        }
     }
 }
