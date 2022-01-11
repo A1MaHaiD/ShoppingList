ec8c92a261a78338b316532cf96bcbc94e54f568
A1MaHaiD <super.lukianchuk2010@gmail.com>
2022-01-11 02:52:20 +0200

    refactoring code... && added fieldInit() && parseParam()

--- app/src/main/java/com/handroid/shoppinglist/presentation/ui/fragments/ShopItemFragment.kt
+++ app/src/main/java/com/handroid/shoppinglist/presentation/ui/fragments/ShopItemFragment.kt
@@ -1,15 +1,31 @@
 package com.handroid.shoppinglist.presentation.ui.fragments
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.google.android.material.textfield.TextInputEditText
+import com.handroid.shoppinglist.R
 import com.handroid.shoppinglist.databinding.FragmentShopItemBinding
+import com.handroid.shoppinglist.domain.ShopItem
+import com.handroid.shoppinglist.presentation.ui.activities.ShopItemActivity
+import com.handroid.shoppinglist.presentation.view_models.ShopItemViewModel
 
-class ShopItemFragment : Fragment() {
+class ShopItemFragment(
+    private val screenMode: String = MODE_UNKNOWN,
+    private val shopItemId: Int = ShopItem.UNDEFINED_ID
+) : Fragment() {
 
-    lateinit var binding: FragmentShopItemBinding
+    private lateinit var binding: FragmentShopItemBinding
+    private lateinit var viewModel: ShopItemViewModel
+    private lateinit var fieldName: TextInputEditText
+    private lateinit var fieldCount: TextInputEditText
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -18,5 +34,132 @@ class ShopItemFragment : Fragment() {
     ): View? {
         binding = FragmentShopItemBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        parseParams()
+        viewModel = ViewModelProvider(this)[ShopItemViewModel::class.java]
+        fieldInit()
+        addTextChangeListeners()
+        launchRightMode()
+        observeViewModel()
+    }
+
+    private fun fieldInit() {
+        fieldName = binding.tiEtName
+        fieldCount = binding.tiEtCount
+    }
+
+    private fun observeViewModel() {
+        viewModel.errorInputName.observe(viewLifecycleOwner) {
+            val errorMessage = if (it) {
+                getString(R.string.error_input_name)
+            } else {
+                null
+            }
+            binding.tilName.error = errorMessage
+        }
+        viewModel.errorInputCount.observe(viewLifecycleOwner) {
+            val errorMessage = if (it) {
+                getString(R.string.error_input_count)
+            } else {
+                null
+            }
+            binding.tilCount.error = errorMessage
+        }
+        viewModel.shouldCloseScreen.observe(viewLifecycleOwner) {
+            isHidden
+//            finish()
+        }
+    }
+
+    private fun launchRightMode() {
+        when (screenMode) {
+            MODE_EDIT -> launchEditMode()
+            MODE_ADD -> launchAddMode()
+        }
+    }
+
+    private fun addTextChangeListeners() {
+        fieldName.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                viewModel.resetErrorInputName()
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+            }
+        })
+        fieldCount.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                viewModel.resetErrorInputCount()
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+            }
+        })
+    }
+
+    private fun launchEditMode() {
+        viewModel.getShopItem(shopItemId)
+        viewModel.shopItem.observe(viewLifecycleOwner) {
+            fieldName.setText(it.name)
+            fieldCount.setText(it.count.toString())
+        }
+        binding.btnSave.setOnClickListener {
+            viewModel.editShopItem(
+                fieldName.text?.toString(),
+                fieldCount.text?.toString()
+            )
+        }
+    }
+
+    private fun launchAddMode() {
+        binding.btnSave.setOnClickListener {
+            viewModel.addShopItem(
+                fieldName.text?.toString(),
+                fieldCount.text?.toString()
+            )
+        }
+    }
+
+    private fun parseParams() {
+        if (screenMode != MODE_EDIT && screenMode != MODE_ADD){
+            throw RuntimeException("Param screen mode is absent")
+        }
+        if (screenMode == MODE_EDIT && shopItemId == ShopItem.UNDEFINED_ID) {
+                throw RuntimeException("Param shop item id is absent")
+            }
+    }
+
+    companion object {
+        private const val EXTRA_SCREEN_MODE = "extra_mode"
+        private const val EXTRA_SHOP_ITEM_ID = "extra_shop_item_id"
+        private const val MODE_EDIT = "mode_edit"
+        private const val MODE_ADD = "mode_add"
+        private const val MODE_UNKNOWN = ""
+
+        fun newIntentAddItem(context: Context): Intent {
+            val intent = Intent(
+                context, ShopItemActivity::class.java
+            )
+            intent.putExtra(EXTRA_SCREEN_MODE, MODE_ADD)
+            return intent
+        }
+
+        fun newIntentEditItem(context: Context, shopItemId: Int): Intent {
+            val intent = Intent(
+                context, ShopItemFragment::class.java
+            )
+            intent.putExtra(EXTRA_SCREEN_MODE, MODE_EDIT)
+            intent.putExtra(EXTRA_SHOP_ITEM_ID, shopItemId)
+            return intent
+        }
     }
 }
